2d2c9fb6965ce02955d3c968a9c79953e41aa481
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2022-09-29 21:58:29 -0400

    fixing documentation

--- R/fv_p_chart.R
+++ R/fv_p_chart.R
@@ -6,10 +6,12 @@
 #' @export
 #'
 #' @importFrom shewhart.hybrid plot_run_chart P_Chart
+#' @importFrom dplyr rename
+#' @importFrom shewhart.hybrid plot_run_chart P_Chart
 fv_p_chart = function(dataset = fv)
 {
   dataset |>
-    rename(n = `Total A&B`, N = `TOTAL SPECIMENS`) |>
+    dplyr::rename(n = `Total A&B`, N = `TOTAL SPECIMENS`) |>
     shewhart.hybrid::P_Chart() |>
     shewhart.hybrid::plot_run_chart()
 }
